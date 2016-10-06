bdd70ab5e2feaa012a44580690cf8b1aacaeb71b
Mikel Duffy <mduffy23@gmail.com>
2016-10-06 05:32:32 -0700

    chore: configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname  + '/dist',
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }},
+      {test: /\.css$/, loader: 'style-loader!css-loader'}
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
